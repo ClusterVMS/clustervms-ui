1f0eb73ccc0d6a63d82a8992a6b270d43ac5fcde
Alicrow <writerofalicrow@gmail.com>
2022-12-17 12:55:42 -0500

    Adaptive video grid layout that dynamically determines optimal arrangement. Also removed the "ClusterVMS UI" header text left over from original Angular skeleton.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import { Camera } from './camera';
 import { CameraService } from './camera.service';
@@ -6,12 +6,19 @@ import { CameraService } from './camera.service';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	styleUrls: ['./app.component.scss'],
+	host: {
+		'(window:resize)': 'onResized($event)'
+	}
 })
 export class AppComponent {
 	title = 'clustervms-ui';
 
 	cameras: Camera[] = [];
+
+	videoColumns = 1;
+
+	@ViewChild('videoGrid') videoGridElement!: ElementRef;
 
 	constructor(private cameraService: CameraService) {}
 
@@ -19,10 +26,73 @@ export class AppComponent {
 		this.getCameras();
 	}
 
+	onResized(event: any) {
+		this.updateGrid();
+	}
+
+	/// Updates the number of rows and columns to best fit the videos
+	updateGrid(): void {
+		let element = this.videoGridElement.nativeElement;
+		let gridAspectRatio = element.offsetParent.clientWidth / element.offsetParent.clientHeight;
+		let videoAspectRatio = 16 / 9;
+		let numVideos = this.cameras.length;
+
+		// Find how many rows it takes before we're forced to scale down the players to avoid overflowing the height.
+		for(let numRows = 1; numRows <= numVideos; ++numRows) {
+			// working in relative dimensions; grid height is 1, grid width is gridAspectRatio
+			let gridWidth = gridAspectRatio;
+			let gridHeight = 1;
+			let gridArea = gridWidth * gridHeight;
+			let numColumns = Math.ceil(numVideos / numRows);
+
+			let columnWidth = gridWidth / numColumns;
+			let rowHeight = columnWidth / videoAspectRatio;
+			let contentHeight = numRows * rowHeight;
+
+			if(contentHeight > gridHeight) {
+				// Vertical overflow!
+				// This should be the ideal number of columns if filling the height of the grid
+				// If filling the width of the grid, we want one less row
+
+				if(numRows == 1) {
+					// One row is the minimum we can do, so we'll go with that
+					this.videoColumns = numColumns;
+					return;
+				}
+
+				// Calculate fill proportion if filling the height of the grid
+				let playerHeight = gridHeight / numRows;
+				let playerWidth = playerHeight * videoAspectRatio;
+				let fillHeightPlayerArea = playerWidth * playerHeight;
+				let fillHeightFillProportion = (numVideos * fillHeightPlayerArea) / gridArea;
+
+				// Figure out the fill proportion if we used one less row and filled the width of the grid
+				let fillWidthNumRows = numRows - 1;
+				let fillWidthNumColumns = Math.ceil(numVideos / fillWidthNumRows);
+				playerWidth = gridWidth / fillWidthNumColumns;
+				playerHeight = playerWidth / videoAspectRatio;
+				let fillWidthPlayerArea = playerWidth * playerHeight;
+				let fillWidthFillProportion = (numVideos * fillWidthPlayerArea) / gridArea;
+
+				if (fillHeightFillProportion > fillWidthFillProportion) {
+					this.videoColumns = numColumns;
+				} else {
+					this.videoColumns = fillWidthNumColumns;
+				}
+				return;
+			}
+		}
+
+		// Never found a number of rows that would cause vertical overflow
+		// This means we can put everything into one column, expand to fill the width, and still have extra vertical space.
+		this.videoColumns = 1;
+	}
+
 	getCameras(): void {
 		this.cameraService.getCameras().subscribe(cameras => {
 			console.log(cameras);
 			this.cameras = cameras;
+			this.updateGrid();
 		});
 	}
 }
